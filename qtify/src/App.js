8289d8530b669bbbcd9fc187b104976f57d6323e
Deepak Shah <dpakumarshah@gmail.com>
2024-10-22 20:14:53 +0530

    Milestone 1

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+import Logo from "./components/Logo/Logo";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar searchData={[...topAlbum, newAlbum]}>
+        <Logo />
+        <Search placeholder={"Search a album of your choice"} />
+        <Button
+          text="Give Feedback"
+          onClick={() => alert("Feedback button clicked!")}
+        />
+      </Navbar>
+      <Outlet context={{ data: { topAlbum, newAlbum, genres, songs } }} />
+    </>
   );
 }
 
